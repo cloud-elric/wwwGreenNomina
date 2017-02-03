439e4b11aaa3b8fd8961354a20b5667ae9c377db
cloud-elric <humberto@2gom.com.mx>
2017-02-03 14:57:58 -0600

    cambios index

--- views/site/index.php
+++ views/site/index.php
@@ -5,7 +5,7 @@ $this->title = 'Nomina';
 ?>
 <div class="row">
 	<div class="col-md-4">
-		<a href="">
+		<a href="<?=Yii::$app->urlManager->createAbsoluteUrl ( ['empleados/index'] );?>">
 			<div class="widget widget-shadow" id="widgetLineareaOne">
 				<div class="widget-content">
 					<div class="padding-20 padding-top-10">
@@ -34,67 +34,5 @@ $this->title = 'Nomina';
 			</div>
 		</a>
 	</div>
-	<div class="col-md-4">
-		<a href="">
-			<div class="widget widget-shadow" id="widgetLineareaOne">
-				<div class="widget-content">
-					<div class="padding-20 padding-top-10">
-						<div class="clearfix">
-							<div class="grey-800 pull-left padding-vertical-10">
-								<i class="glyphicon glyphicon-user"></i> Empleados
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-	</div>
 	
-	<div class="col-md-4">
-		<a href="">
-			<div class="widget widget-shadow" id="widgetLineareaOne">
-				<div class="widget-content">
-					<div class="padding-20 padding-top-10">
-						<div class="clearfix">
-							<div class="grey-800 pull-left padding-vertical-10">
-								<i class="glyphicon glyphicon-user"></i> Empleados
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-	</div>
-	
-	<div class="col-md-4">
-		<a href="">
-			<div class="widget widget-shadow" id="widgetLineareaOne">
-				<div class="widget-content">
-					<div class="padding-20 padding-top-10">
-						<div class="clearfix">
-							<div class="grey-800 pull-left padding-vertical-10">
-								<i class="glyphicon glyphicon-user"></i> Empleados
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-	</div>
-	
-	<div class="col-md-4">
-		<a href="">
-			<div class="widget widget-shadow" id="widgetLineareaOne">
-				<div class="widget-content">
-					<div class="padding-20 padding-top-10">
-						<div class="clearfix">
-							<div class="grey-800 pull-left padding-vertical-10">
-								<i class="glyphicon glyphicon-user"></i> Empleados
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-	</div>
 </div>
