18b2016ccd682d7ce0752366f70825c57c7ae2cd
cloud-elric <humberto@2gom.com.mx>
2017-02-07 15:59:11 -0600

    Se agrego la pantalla para ver a los empleados pagados por tiempo

--- views/site/index.php
+++ views/site/index.php
@@ -34,5 +34,19 @@ $this->title = 'Nomina';
 			</div>
 		</a>
 	</div>
-	
+	<div class="col-md-4">
+		<a href="<?=Yii::$app->urlManager->createAbsoluteUrl ( ['empleados/pagos'] );?>">
+			<div class="widget widget-shadow" id="widgetLineareaOne">
+				<div class="widget-content">
+					<div class="padding-20 padding-top-10">
+						<div class="clearfix">
+							<div class="grey-800 pull-left padding-vertical-10">
+								<i class="glyphicon glyphicon-usd"></i> Pagos
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</a>
+	</div>
 </div>
